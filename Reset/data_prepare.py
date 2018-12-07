9418ab13af0f6088f9263008b937cc1350c805d7
hexy717@163.com <hexy717@163.com>
2018-12-07 15:00:41 -0600

    .

--- Reset/data_prepare.py
+++ Reset/data_prepare.py
@@ -1,33 +1,47 @@
 from PIL import Image
 import glob
-# from resizeimage import resizeimage
-from keras import backend as K
-from six.moves import cPickle
-from keras.datasets import cifar10
+import os
+#from resizeimage import resizeimage
+#from keras import backend as K
+#from six.moves import cPickle
+#from keras.datasets import cifar10
 import numpy as np
 import csv
 import codecs
 import random
-
+from skimage.transform import resize
+from skimage import io
 import re
-r = re.compile("\d+")
-def key1(a):
-    m = r.findall(a)
-    return int(m[0]), int(m[1])
 
-# #resize images
-# def resize_Images():
-#     for filepath in glob.iglob('dataset/*.jpg'):
-#         with open(filepath, 'r+b') as f:
-#             with Image.open(f) as image:
-#                 resized = resizeimage.resize_contain(image, [32, 32])
-#                 resized.save('resized/' + filepath, image.format)
+image_size = 224
+
+r1 = re.compile("frame_(\d+)")
+r2 = re.compile("patch_(\d+)")
+def key1(a):
+    m1 = r1.findall(a)
+    m2 = r2.findall(a)
+    return int(m1[0]), int(m2[0])
+
+#resize images
+def resize_Images():
+    for filepath in glob.iglob('data/original/*.jpg'):
+        filename = os.path.basename(filepath)
+
+        image = io.imread(filepath)
+        resized = resize(image, (image_size, image_size), anti_aliasing=True)        
+        io.imsave('dataset/resized224/' + filename, resized)
+
+        #with open(filepath, 'r+b') as f:
+            #with Image.open(f) as image:
+                #resized = resizeimage.resize_crop(image, [image_size, image_size])
+                #resized.save('dataset/resized224/' + filename, image.format)
+        
 
 def load_Data():
-    num_train_samples = 2274
+    num_train_samples = 2273
     testset_size = 450
 
-    x_train = np.empty((num_train_samples, 32, 32, 3), dtype='uint8')
+    x_train = np.empty((num_train_samples, image_size, image_size, 3), dtype='uint8')
     y_train = np.empty((num_train_samples,), dtype='uint8')
 
     (all_x, all_y) = load_batch()
@@ -47,7 +61,7 @@ def load_Data():
             x_train.append(all_x[index])
 	
     x_train = np.array(x_train)
-    x_test = np.array(x_test)
+    x_test = np.array(x_test)	
 
     y_train = np.reshape(y_train, (len(y_train), 1))
     y_test = np.reshape(y_test, (len(y_test), 1))
@@ -57,17 +71,17 @@ def load_batch():
     #image data
     imageCount = 0
     filelist = []
-    for imagefile in glob.iglob('dataset/resized_dataset/*.jpg'):
+    for imagefile in glob.iglob('data/resized224/*.jpg'):
         filelist.append(imagefile)
         imageCount += 1
 
     filelist.sort(key = key1)
-    data = np.zeros((imageCount, 32, 32, 3))
+    data = np.zeros((imageCount, image_size, image_size, 3))
     labels = np.zeros(imageCount)
     index = 0
 
-    for imagefile in filelist:
-        print(imagefile)
+    for counter, imagefile in enumerate(filelist):
+        print(str(counter + 1) + 'load image file: ' + imagefile)
         t = Image.open(imagefile)
         arr = np.array(t) #Convert test image into an array 32*32*3    
         data[index] = arr 
@@ -76,9 +90,10 @@ def load_batch():
     #labels
     labels = []
     #for filepath in glob.iglob(''):
-    with open('dataset/labels/all.txt') as csvfile:
+    with open('data/labels/all.txt') as csvfile:
         lines = csvfile.readlines()
         for line in lines:
             labels.append(line.strip().split(',')[1])    
     return data, labels
 
+# load_Data()
